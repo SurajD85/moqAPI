8746a4efa176d4b98e19aa479e091ce13c0a6aca
edocfifty <jarusdeuja@gmail.com>
2024-12-22 18:16:02 +0545

    Remove .vs folder and update .gitignore and added folder structure

---
+++ Moq.Business/Repository/Candidate/CandidateRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moq.Business.Repository.Candidate
+{
+    internal class CandidateRepository
+    {
+    }
+}
---
+++ Moq.DB/Context/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moq.DB.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+    // Candidate Entity
+    public class Candidate
+    {
+        [Key]
+        public string Email { get; set; } // Unique identifier
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string CallTimeInterval { get; set; }
+        public string LinkedInUrl { get; set; }
+        public string GitHubUrl { get; set; }
+
+        [Required]
+        public string Comment { get; set; }
+    }
+}
